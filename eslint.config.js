1a30cceb47ad5461d96a96b61674ca1021b43349
NVH19 <vanhoam10@gmail.com>
2025-10-02 15:10:22 +0700

    configure project

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,45 @@
+import eslintPluginPrettier from 'eslint-plugin-prettier'
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  { ignores: ['dist', 'vite.config.ts'] },
   {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
     },
-  },
-])
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier: eslintPluginPrettier
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      '@typescript-eslint/no-unused-vars': 'warn', // Disable rule này
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react-hooks/exhaustive-deps': 'warn', // Đảm bảo bạn vẫn cảnh báo thiếu dependencies
+      'max-lines-per-function': ['off'], // (Tùy chọn) nếu bạn bị giới hạn số dòng code trong một hàm
+      'prettier/prettier': [
+        'warn',
+        {
+          arrowParens: 'always',
+          semi: false,
+          trailingComma: 'none',
+          tabWidth: 2,
+          endOfLine: 'auto',
+          useTabs: false,
+          singleQuote: true,
+          printWidth: 120,
+          jsxSingleQuote: true
+        }
+      ]
+    }
+  }
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,52 @@
 import { defineConfig } from 'vite'
+import path from 'path'
+import svgr from 'vite-plugin-svgr'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import envCompatible from 'vite-plugin-env-compatible'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    envCompatible(),
+    svgr({
+      svgrOptions: {
+        icon: true // Đảm bảo SVG được render như một React Component
+      }
+    }),
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: '__tla',
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: (i) => `__tla_${i}`
+    })
+  ],
+  build: {
+    minify: 'terser', // Dùng terser để minify mã
+    cssCodeSplit: true, // Tách riêng CSS
+    chunkSizeWarningLimit: 1500, //
+    target: 'esnext' //browsers can handle the latest ES features
+  },
+  server: {
+    port: 3000,
+    hmr: true,
+    proxy: {
+      // Proxy all requests from /api to http://localhost:8082/api
+      '/api': {
+        target: 'http://localhost:8082',
+        changeOrigin: true,
+        secure: false, // Nếu API chạy trên http, bạn có thể đặt `secure: false`
+        rewrite: (path) => path.replace(/^\/api/, '/api') // Sửa URL khi proxy
+      }
+    }
+  },
+  css: {
+    devSourcemap: true
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
+  }
 })
