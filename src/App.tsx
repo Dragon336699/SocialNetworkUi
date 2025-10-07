e46d7965f861b183d2eb49b670aa655b87d77a89
Bảo Long <anhruoia1a1@gmail.com>
2025-10-07 16:02:25 +0700

    Add confirmation email page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import Login from './app/pages/Login/Login'
+import EmailConfirm from './app/pages/EmailConfirm/EmailConfirm'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/Login' element={<Login />}></Route>
+        <Route path='/EmailConfirm/:status' element={<EmailConfirm />}></Route>
       </Routes>
     </BrowserRouter>
   )
