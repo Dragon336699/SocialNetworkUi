e8f3b4d70be9238c59f2b5c65c635e26e5097123
NVH19 <vanhoam10@gmail.com>
2025-11-23 18:27:45 +0700

    send friend request

--- src/app/services/relation.service.ts
+++ src/app/services/relation.service.ts
@@ -1,9 +1,37 @@
 import { apiClient } from '../environments/axiosClient'
 import { BaseResponse } from '../types/Base/Responses/baseResponse'
+import { SentFriendRequestData } from '../types/Relations/relations'
 
 export const relationService = {
   async addFriend(userId: string): Promise<{ data: BaseResponse; status: number }> {
-    const response = await apiClient.post<BaseResponse>(`user/friend-request/send`, userId, {
+    const response = await apiClient.post<BaseResponse>(
+      `/friend-request/send`,
+      { receiverId: userId },
+      {
+        withCredentials: true
+      }
+    )
+    return { data: response.data, status: response.status }
+  },
+
+  async cancelFriendRequest(userId: string): Promise<{ data: BaseResponse; status: number }> {
+    const response = await apiClient.post<BaseResponse>(
+      `/friend-request/cancel`,
+      { receiverId: userId },
+      {
+        withCredentials: true
+      }
+    )
+    return { data: response.data, status: response.status }
+  },
+
+  async getSentFriendRequest(
+    userId: string,
+    skip?: number,
+    take?: number
+  ): Promise<{ data: SentFriendRequestData[]; status: number }> {
+    const response = await apiClient.get<SentFriendRequestData[]>(`/friend-request/sent/${userId}`, {
+      params: { skip, take },
       withCredentials: true
     })
     return { data: response.data, status: response.status }
