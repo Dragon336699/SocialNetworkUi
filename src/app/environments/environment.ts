d770be9d08596e96263a5028a5e69cc65a0dac07
Bảo Long <anhruoia1a1@gmail.com>
2026-01-02 01:18:38 +0700

    change link to vps

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'https://backend-dotnet:44302/api/v1/'
-export const CHAT_HUB_URL = 'https://backend-dotnet:44302/chathub'
+export const API_BASE_URL = 'http://72.62.78.5:5000/api/v1/'
+export const CHAT_HUB_URL = 'http://72.62.78.5:5000/chathub'
 export const API_AI_URL = 'http://backend-python:8000/'
