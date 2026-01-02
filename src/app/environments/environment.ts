899265450d752511b0bc3d62b5bdeb16a835d9d2
Bảo Long <anhruoia1a1@gmail.com>
2026-01-02 20:41:42 +0700

    fix http to https

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://72.62.78.5:5000/api/v1/'
-export const CHAT_HUB_URL = 'http://72.62.78.5:5000/chathub'
-export const API_AI_URL = 'http://72.62.78.5:8000/'
+export const API_BASE_URL = 'https://fricon.online:5000/api/v1/'
+export const CHAT_HUB_URL = 'https://fricon.online:5000/chathub'
+export const API_AI_URL = 'https://fricon.online:8000/'
