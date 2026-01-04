5f6ad3f79e476e96aac2322a771e02aec49d5482
Bảo Long <anhruoia1a1@gmail.com>
2026-01-04 17:54:01 +0700

    add CI/CD file

--- src/app/common/Assests/CommonVariable.tsx
+++ src/app/common/Assests/CommonVariable.tsx
@@ -1,2 +1,4 @@
 export const DEFAULT_AVATAR_URL =
   'https://res.cloudinary.com/dhnjbohwa/image/upload/v1766045971/default-avatar_ko3tz9.jpg'
+export const CHATBOT_AVATAR_URL =
+  'https://res.cloudinary.com/dhnjbohwa/image/upload/v1767522478/chatbot_avatar_qckgwm.jpg'
